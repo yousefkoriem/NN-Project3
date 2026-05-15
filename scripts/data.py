921fe3dfaf0efe9e7d6252d34fd5ca74d7eb62b2
Yousef Koriem <koriemyousef@gmail.com>
2026-05-15 19:59:45 +0300

    data script

---
+++ scripts/data.py
@@ -0,0 +1,15 @@
+import os
+from keras.utils import to_categorical
+from keras.datasets import cifar10
+import numpy as np
+import pickle
+
+os.chdir('/home/bluefox/NN/NN-Project3')
+(x_train, y_train), (x_test,y_test) = cifar10.load_data()
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+y_train = keras.utils.to_categorical(y_train,num_classes=10)
+y_test = keras.utils.to_categorical(y_test,num_classes=10)
+pickle.dump(obj=(x_train,y_train),file=open('data/processed/train_data.pkl', 'wb'))
+pickle.dump(obj=(x_test,y_test),file=open('data/processed/test_data.pkl', 'wb'))
